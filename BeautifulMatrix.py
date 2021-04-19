f5086fd06d25a3b1f37c1eea518c56950016420b
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 13:45:20 +0600

    Add files via upload

---
+++ BeautifulMatrix.py
@@ -0,0 +1,12 @@
+matrix = []
+for i in range(5):
+      matrix.append(list(map(int, input().split())))
+
+for i, mat in enumerate(matrix):
+      for idx, lst in enumerate(mat):
+            if matrix[i][idx] == 1:
+                  x1, y1 = i, idx
+
+x2, y2 = 2,2
+
+print(abs(x1-x2)+abs(y1-y2))
