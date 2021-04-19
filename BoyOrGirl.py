300fbfadcffe23cd49eda25ba53339092a92e1ba
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 18:30:06 +0600

    Add files via upload

---
+++ BoyOrGirl.py
@@ -0,0 +1,12 @@
+__author__ = 'shukkkur'
+
+'''
+https://codeforces.com/problemset/problem/236/A
+'''
+
+username = set(input())
+
+if len(username) % 2 == 1:
+      print("IGNORE HIM!" )
+else:
+      print("CHAT WITH HER!")
