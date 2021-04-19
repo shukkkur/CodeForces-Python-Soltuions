82e7f5f29c5bd50705d4b56f52983f3783412a4c
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 13:40:19 +0600

    Update and rename dominoPiling.py to DominoPiling.py

---
+++ DominoPiling.py
@@ -0,0 +1,9 @@
+__author__ = 'shukkkur'
+
+'''
+https://codeforces.com/problemset/problem/50/A
+'''
+
+m, n = map(int, input().split())
+square = m*n
+print(square//2)
--- dominoPiling.py
+++
@@ -1,3 +0,0 @@
-m, n = map(int, input().split())
-square = m*n
-print(square//2)
