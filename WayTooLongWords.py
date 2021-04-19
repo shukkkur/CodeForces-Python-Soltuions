34d5c27a592a1a31f970188ad94345611a2113db
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 13:30:11 +0600

    Add files via upload

---
+++ WayTooLongWords.py
@@ -0,0 +1,10 @@
+testCase = int(input())
+
+for _ in range(testCase):
+      word = input()
+      if len(word) > 10:
+            short = word[:1] + str(len(word[1:-1])) + word[-1:]
+            print(short)
+      else:
+            print(word)
+      
