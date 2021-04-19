0c5b1c167b173caa908acf4ddf91f2c69f7e76dc
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 14:40:14 +0600

    Add files via upload

---
+++ StonesOnTheTable.py
@@ -0,0 +1,14 @@
+__author__ = 'shukkkur'
+
+'''
+https://codeforces.com/problemset/problem/266/A
+'''
+
+num = int(input())
+stones = list(input()) # RGBBR
+count = 0
+for i in range(len(stones)-1):
+      if stones[i] == stones[i+1]:
+            count += 1
+
+print(count)
