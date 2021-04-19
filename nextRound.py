e0b283d2b4a12a097490314a4defec549a172902
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-19 13:37:35 +0600

    Add files via upload

---
+++ nextRound.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+scores = [int(item) for item in input().split()]
+
+count = 0
+idx = scores[k - 1]
+for score in scores:
+      if score >= idx and score > 0:
+            count += 1
+            
+print(count)
