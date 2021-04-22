b242b467803784144949b5b0b5292788a804d862
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-23 02:50:28 +0600

    977A

---
+++ WrongSubtraction.py
@@ -0,0 +1,14 @@
+__author__ = 'shukkkur'
+
+'''
+https://codeforces.com/problemset/problem/977/A
+'''
+
+n, k = map(int, input().split())
+
+for _ in range(k):
+      if n % 10 == 0:
+            n /= 10
+      else:
+            n -= 1
+print(int(n))
