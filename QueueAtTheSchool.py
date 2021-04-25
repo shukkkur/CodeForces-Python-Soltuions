fcd62ce8790e9dd8b148add99c1e29d7cb539b1e
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-25 15:42:31 +0300

    266B

---
+++ QueueAtTheSchool.py
@@ -0,0 +1,25 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/266/B
+'''
+
+
+n, t = map(int, input().split())
+queue = list(input())
+
+for _ in range(t):
+      was = False
+      for idx, student in enumerate(queue):
+            if student == 'B' and idx != len(queue)-1 and queue[idx+1] == 'G' and was == False:
+                  queue[idx], queue[idx+1] = queue[idx+1], queue[idx]
+                  was = True
+            else:
+                  was = False
+            
+
+print(''.join(queue))
+
+
+
