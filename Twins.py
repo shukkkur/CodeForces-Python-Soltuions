0f83449a0593b5f7e6d48d1f7126dc6b92c42910
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-26 12:33:53 +0300

    160A

---
+++ Twins.py
@@ -0,0 +1,21 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/160/A
+'''
+
+
+n = int(input())
+arr = sorted(list(map(int, input().split())), reverse=True)
+
+twin1 = 0
+count = 0
+
+while twin1 <= sum(arr):
+      twin1 += arr.pop(0)
+      count += 1
+print(count)
+      
+
+
