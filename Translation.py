94fd702bff87ed2994112c9f55f83bc274aeb83a
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-26 23:14:08 +0300

    41A

---
+++ Translation.py
@@ -0,0 +1,16 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/41/A
+'''
+
+
+berlandish = input()
+birlandish = input()
+
+if birlandish == berlandish[::-1]:
+      print('YES')
+else:
+      print('NO')
+
