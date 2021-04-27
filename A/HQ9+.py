b2fad88e518d973e07b918e88fc27db1dc4dc85a
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-04-27 23:48:12 +0300

    133A

---
+++ A/HQ9+.py
@@ -0,0 +1,16 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/133/A
+'''
+
+
+p = input()
+
+
+if ('H' in p) or ('Q' in p) or ('9' in p):
+      print('YES')
+else:
+      print('NO')
+
