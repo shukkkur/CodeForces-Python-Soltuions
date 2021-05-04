138e2cb0447fbaa5856de64d9527398189d217ad
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-05-04 21:36:51 +0600

    Update CalculatingFunction.py

--- A/CalculatingFunction.py
+++ A/CalculatingFunction.py
@@ -7,14 +7,10 @@ https://codeforces.com/problemset/problem/486/A
 
 
 n = int(input())
-count = 0
 
-for i in range(1, n+1):
-      if i % 2 != 0:
-            count += -i
-      else:
-            count += i
-
-print(count)
+if n % 2 == 0:
+      print(n // 2)
+else:
+      print(-1*(n+1)//2)
 
 
