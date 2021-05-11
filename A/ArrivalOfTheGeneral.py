004fb0499c772a990f9b1d8498dc762fc9e30646
Shukur <78250180+shukkkur@users.noreply.github.com>
2021-05-11 23:37:27 +0300

    144A - Arrival of the General

---
+++ A/ArrivalOfTheGeneral.py
@@ -0,0 +1,23 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/144/A
+'''
+
+
+n = int(input())
+order = list(map(int, input().split()))
+secs = 0
+
+# Max & Min Location
+idxMin = len(order) - order[::-1].index(min(order)) - 1
+idxMax = order.index(max(order))
+
+
+secs = idxMax + n - 1- idxMin
+
+if idxMax > idxMin:
+      secs -= 1
+
+print(secs)
