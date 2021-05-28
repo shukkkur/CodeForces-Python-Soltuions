8386ba2cba95d356b1a07eb623e28177a5aba59d
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-05-28 23:55:24 +0600

    1154A - Restoring Three Numbers

--- A/RestoringThreeNumbers.py
+++ A/RestoringThreeNumbers.py
@@ -4,6 +4,14 @@ __author__ = 'shukkkur'
 '''
 https://codeforces.com/problemset/problem/1154/A
 A. Restoring Three Numbers
+
+System of equations => 3 unknowns and 4 equations:
+1) a + b = x        | a = x - b     |                   |
+2) a + c = y        | a = y - c     | a = y - c         |
+3) b + c = z        | b = z - c     | b = z - c         |
+4) a + b + c = t    | a = t - b - c | x - b = t - b - c | c = t - x 
+
+Since the input is given in a mixed order, we only care about the t = a + b + c, which is the max of all the input numbers 
 '''
 
 
