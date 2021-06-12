ed362a87497a4396d7d0d6a98d813c1b0ffb9959
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-06-12 13:40:16 +0300

    A - Mishka and Game

---
+++ A/MishkaAndGame.py
@@ -0,0 +1,23 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/703/A
+A. Mishka and Game
+'''
+
+n = int(input())
+m = 0
+c = 0
+for i in range(n):   
+    a,b = [int(i) for i in input().split()]
+    if a > b:
+        m += 1
+    if b > a:
+        c += 1
+if m > c:
+    print('Mishka')
+if c > m:
+    print('Chris')
+if c == m:
+    print('Friendship is magic!^^')
