4726db1ca40387ed300378965cae9bad1e4657da
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-06-17 15:38:43 +0600

    1348A - Phoenix and Balance

--- A/PhoenixAndBalance.py
+++ A/PhoenixAndBalance.py
@@ -4,6 +4,10 @@ __author__ = 'shukkkur'
 '''
 https://codeforces.com/problemset/problem/1348/A
 A. Phoenix and Balance
+
+The general pattern is: 
+One list has the has n/2 coins one of which is the coin with the maximum weight, whereas the the other coins (n/2-1 coins) are the coins with minimum weight. 
+The second list has the remaining coins.
 '''
 
 
@@ -15,7 +19,7 @@ for _ in range(t):
       coins = [weight(i) for i in range(1, n+1)]
       a = []
       maxy = True
-      for i in range(n//2):
+      for i in range(n//2): # this loop creates the first list which consists of one coin with max weight and the remaining are have min weight
             if maxy:
                   a.append(coins.pop(coins.index(max(coins))))
                   maxy = False
@@ -23,6 +27,3 @@ for _ in range(t):
                   a.append(coins.pop(coins.index(min(coins))))
       
       print(sum(a) - sum(coins))
-
-
-      
