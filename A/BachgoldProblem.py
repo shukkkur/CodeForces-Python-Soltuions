0ea184a9057af102c8c47bcd77cd1ba438c2ebba
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-06-19 14:58:05 +0600

    Update BachgoldProblem.py

--- A/BachgoldProblem.py
+++ A/BachgoldProblem.py
@@ -9,15 +9,11 @@ A. Bachgold Problem
 
 n = int(input())
 
+print(n//2)
 if n % 2 == 0:
-      print(n//2)
       for i in range(n//2):
             print(2, end= ' ')
 else:
-      print(n//2)
       for i in range(n//2-1):
             print(2, end=' ')
       print(3)
-
-
-
