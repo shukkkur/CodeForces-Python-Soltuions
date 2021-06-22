669dd83bc9fc7b148b8d9edd4eff2f3ea20b654c
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-06-22 10:57:45 +0300

    1370A - Maximum GCD

---
+++ A/MaximumGCD.py
@@ -0,0 +1,14 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1370/A
+A. Maximum GCD
+'''
+
+t = int(input())
+
+for _ in range(t):
+      n = int(input())
+      print(n//2)
+
