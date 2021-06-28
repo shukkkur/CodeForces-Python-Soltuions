5970f79db56117ab4e94ca2e1b836356458b7249
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-06-28 10:45:46 +0300

    1374B - Multiply by 2, divide by 6

---
+++ B/MultiplyBy2,DivideBy6.py
@@ -0,0 +1,25 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1374/B
+B. Multiply by 2, divide by 6
+'''
+
+
+t = int(input())
+
+for _ in range(t):
+      n = int(input())
+      count = 0
+      while n > 1 and n <= 10**9:
+            if n % 6 == 0:
+                  n //= 6
+            else:
+                  n *= 2
+            count += 1
+
+      print(-1) if n >= 10**9 else print(count)
+
+
+      
