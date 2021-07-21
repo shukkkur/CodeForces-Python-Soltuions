a2ca9aa46e8a990c3e7dba13f0bb330743e9cf35
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-07-21 22:48:41 +0300

    151A - Soft Drinking

---
+++ A/SoftDrinking.py
@@ -0,0 +1,15 @@
+__author__ = 'shukkkur'
+
+
+'''
+A. Soft Drinking
+https://codeforces.com/problemset/problem/151/A
+'''
+
+n, k, l, c, d, p, nl, np = map(int, input().split())
+
+ml = (k * l)//nl
+slices = c * d
+salt = p // np
+
+print(min(ml, slices, salt)//n)
