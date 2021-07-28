a5c6868c2bf15473a81e3717631dde27174ea472
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-07-28 10:48:04 +0300

    Add files via upload

---
+++ A/SpecialPermutation.py
@@ -0,0 +1,20 @@
+__author__ = 'shukkkur'
+
+
+'''
+A. Special Permutation
+https://codeforces.com/problemset/problem/1454/A
+'''
+
+
+t = int(input())
+
+for _ in range(t):
+      p = [*range(int(input()), 0, -1)]
+      if len(p) > 2:
+            p[len(p)//2], p[len(p)//2 + 1] = p[len(p)//2 + 1], p[len(p)//2]
+            print(*p)
+      else:
+            print(*p)
+      
+      
