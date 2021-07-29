60a978a8ebae4038478613375c909fa96b2f2a77
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-07-29 09:29:23 +0300

    707A - Brain's Photos

---
+++ A/Brain'sPhotos.py
@@ -0,0 +1,25 @@
+__author__ = 'shukkkur'
+
+
+'''
+A. Brain's Photos
+https://codeforces.com/problemset/problem/707/A
+'''
+
+
+n, m = map(int, input().split())
+colored = False
+
+for _ in range(n):
+      colors = set(input().split())
+      for color in colors:
+            if 'W' != color and 'B' != color and  'G' != color:
+                  colored = True
+                  break
+            if colored:
+                  break
+            
+if colored:
+      print('#Color')
+else:
+      print('#Black&White')
