a08f6b05f33e12b186b3bd950f75bd44d26bc5ca
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-04 11:58:34 +0300

    1294A - Collecting Coins

---
+++ A/CollectingCoins.py
@@ -0,0 +1,22 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1294/A
+A. Collecting Coins
+'''
+
+t = int(input())
+
+for _ in range(t):
+      a, b, c, n = map(int, input().split())
+      total = a + b + c + n
+      
+      if (a + b + c + n) % 3 == 0 and max(a,b,c) <= total // 3:
+            print('YES')
+      else:
+            print('NO')
+
+
+
+      
