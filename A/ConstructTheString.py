ee66a679adb2a560f1545e9665e1b457600209f8
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-09 12:07:27 +0300

    1335B - Construct the String

---
+++ A/ConstructTheString.py
@@ -0,0 +1,27 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1335/B
+B. Construct the String
+'''
+      
+
+t = int(input())
+alphabet = [*range(97, 123)]
+
+for _ in range(t):
+      
+      n, a, b = map(int, input().split())
+      s = ''
+      unique = ''
+
+      for char in range(b):
+            unique += chr(alphabet[char])
+      repeated = (unique * a)[:a]
+      final = (repeated * n)[:n]
+      
+      print(final)
+
+
+      
