2d5b927a018680183a275d4d474576b8159314d6
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-16 19:12:23 +0600

    1374C - Move Brackets

--- C/MoveBrackets.py
+++ C/MoveBrackets.py
@@ -16,16 +16,3 @@ for _ in range(t):
       while '()' in s:
             s = s.replace('()', '')
       print(s.count('(') )
-##      temp = 0
-##      steps = 0
-##      for bracket in s:
-##            if bracket == '(':
-##                  temp += 1
-##            else:
-##                  temp -= 1
-##                  if temp < 0:
-##                        steps += 1
-##      print(steps)
-
-
-      
