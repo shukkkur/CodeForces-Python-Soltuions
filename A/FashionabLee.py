f1ef6e94310cdc18ea9bb5161935986c3ea83b69
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-17 12:51:58 +0300

    1369A - FashionabLee

---
+++ A/FashionabLee.py
@@ -0,0 +1,24 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1369/A
+A. FashionabLee
+'''
+
+
+t = int(input())
+yes = 'YES'
+no = 'NO'
+
+for _ in range(t):
+      n = int(input())
+      if n % 4 == 0:
+            print(yes)
+      else:
+            print(no)
+
+
+
+      
+      
