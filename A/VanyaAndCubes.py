0b51c796320fa86a6705415f5cf1410555a79e44
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-19 12:03:28 +0300

    492A - Vanya and Cubes

---
+++ A/VanyaAndCubes.py
@@ -0,0 +1,28 @@
+__author__ = 'shukkkur'
+
+
+'''
+A. Vanya and Cubes
+https://codeforces.com/problemset/problem/492/A
+'''
+
+
+n = int(input())
+base = 1
+c = 2
+t = 0
+
+while n - base >= 0:
+      n -= base
+      base += c
+      c += 1
+      t +=1
+
+print(t)
+      
+
+
+
+
+
+
