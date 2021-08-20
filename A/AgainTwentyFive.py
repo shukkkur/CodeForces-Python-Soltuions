d004dadfc2de646b151c563358d2b0abc0578b36
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-20 16:02:40 +0300

    630A - Again Twenty Five!

---
+++ A/AgainTwentyFive.py
@@ -0,0 +1,17 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/630/A
+A. Again Twenty Five!
+'''
+
+
+n = int(input())
+
+if n == 0:
+      print(1)
+elif n == 1:
+      print(5)
+else:
+      print(25)
