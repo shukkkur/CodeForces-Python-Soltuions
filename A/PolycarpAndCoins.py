ceefcf47c79f5f15469a75c02ac5fb208ffd1d9e
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-08-27 22:07:39 +0300

    1551A - Polycarp and Coins

---
+++ A/PolycarpAndCoins.py
@@ -0,0 +1,19 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1551/A
+A. Polycarp and Coins
+'''
+
+
+t = int(input())
+
+for _ in range(t):
+      n = int(input())
+      c2 = round(n / 3)
+      c1 = n - 2*c2
+      print(c1, c2)
+
+
+      
