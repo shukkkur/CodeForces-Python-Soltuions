7004c16da437ebae115162f904de73832f75d9fd
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-09-14 11:35:33 +0600

    1560A - Dislike of Threes

---
+++ A/DislikeOfThrees.py
@@ -0,0 +1,30 @@
+__author__ = 'shukkkur'
+
+
+'''
+https://codeforces.com/problemset/problem/1560/A
+A. Dislike of Threes
+'''
+
+
+t = int(input())
+
+for _ in range(t):
+      k = int(input())
+      c = 0
+      for n in range(1, 3000):
+            if n % 3 != 0 and str(n)[-1] != '3': 
+                  c += 1
+                  if c == k:
+                        print(n)
+                        break
+            
+
+
+
+
+
+
+
+
+      
